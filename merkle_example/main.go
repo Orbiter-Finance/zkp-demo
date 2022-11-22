5ef8efe5e28362ccf93ad6a7b89bf3883cc3ed30
linkdrone <fengwanqing2014@gmail.com>
2022-11-22 18:14:18 +0800

    updates

---
+++ merkle_example/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/consensys/gnark-crypto/accumulator/merkletree"
+	"github.com/consensys/gnark-crypto/ecc"
+	bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/std/accumulator/merkle"
+	"github.com/consensys/gnark/std/hash/mimc"
+	"github.com/joho/godotenv"
+)
+
+type MerkleCircuit struct {
+	Path, Helper []frontend.Variable
+	RootHash     frontend.Variable `gnark:",public"`
+}
+
+func (circuit *MerkleCircuit) Define(api frontend.API) error {
+	hFunc, err := mimc.NewMiMC(api)
+	if err != nil {
+		return err
+	}
+	merkle.VerifyProof(api, hFunc, circuit.RootHash, circuit.Path, circuit.Helper)
+	return nil
+}
+
+func randomStr(length int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
+
+func main() {
+	godotenv.Load(fmt.Sprintf("..%c.env", os.PathSeparator))
+
+	var buf bytes.Buffer
+	for i := 0; i < 10; i++ {
+		buf.Write([]byte(randomStr(10)))
+	}
+
+	// build & verify proof for an elmt in the file
+	proofIndex := uint64(5)
+	segmentSize := 10
+	merkleRoot, merkleProof, numLeaves, err := merkletree.BuildReaderProof(&buf, bn254.NewMiMC(), segmentSize, proofIndex)
+	if err != nil {
+		return
+	}
+	//fmt.Printf("numLeaves: %v\n", numLeaves)
+	//fmt.Printf("proof: %v\n", len(merkleProof))
+
+	proofHelper := merkle.GenerateProofHelper(merkleProof, proofIndex, numLeaves)
+
+	fmt.Printf("ProofHelper: %v\n", proofHelper)
+
+	verified := merkletree.VerifyProof(bn254.NewMiMC(), merkleRoot, merkleProof, proofIndex, numLeaves)
+	if !verified {
+		fmt.Printf("The merkle proof in plain go should pass")
+	}
+
+	// create cs
+	circuit := MerkleCircuit{
+		Path:   make([]frontend.Variable, len(merkleProof)),
+		Helper: make([]frontend.Variable, len(merkleProof)-1),
+	}
+	r1cs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &circuit)
+	if err != nil {
+		fmt.Printf("Compile failed : %v\n", err)
+		return
+	}
+
+	pk, vk, err := groth16.Setup(r1cs)
+	if err != nil {
+		fmt.Printf("Setup failed\n")
+		return
+	}
+
+	assignment := &MerkleCircuit{
+		Path:     make([]frontend.Variable, len(merkleProof)),
+		Helper:   make([]frontend.Variable, len(merkleProof)-1),
+		RootHash: merkleRoot,
+	}
+	for i := 0; i < len(merkleProof); i++ {
+		assignment.Path[i] = merkleProof[i]
+	}
+	for i := 0; i < len(merkleProof)-1; i++ {
+		assignment.Helper[i] = proofHelper[i]
+	}
+	witness, _ := frontend.NewWitness(assignment, ecc.BN254)
+
+	proof, err := groth16.Prove(r1cs, pk, witness)
+	if err != nil {
+		fmt.Printf("Prove failed： %v\n", err)
+		return
+	}
+
+	f, _ := os.OpenFile("merkle_groth16.sol", os.O_CREATE|os.O_WRONLY, 0666)
+	defer f.Close()
+	vk.ExportSolidity(f)
+
+	publicWitness, _ := witness.Public()
+	err = groth16.Verify(proof, vk, publicWitness)
+	if err != nil {
+		fmt.Printf("Verification failed: %v\n", err)
+		return
+	}
+	fmt.Printf("Verification succeded\n")
+}
---
+++ mimc_example/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/std/hash/mimc"
+	"github.com/joho/godotenv"
+)
+
+// CubicCircuit defines a simple circuit
+type CubicCircuit struct {
+	PreImage frontend.Variable
+	Hash     frontend.Variable `gnark:",public"`
+}
+
+// Define declares the circuit constraints
+func (circuit *CubicCircuit) Define(api frontend.API) error {
+	mimc, _ := mimc.NewMiMC(api)
+	mimc.Write(circuit.PreImage)
+	api.AssertIsEqual(circuit.Hash, mimc.Sum())
+	return nil
+}
+
+func mimcHash(data []byte) string {
+	f := bn254.NewMiMC()
+	f.Write(data)
+	hash := f.Sum(nil)
+	hashInt := big.NewInt(0).SetBytes(hash)
+	return hashInt.String()
+}
+
+func main() {
+	godotenv.Load(fmt.Sprintf("..%c.env", os.PathSeparator))
+
+	preImage := []byte{0x01, 0x02, 0x03}
+	hash := mimcHash(preImage)
+
+	fmt.Printf("Hash: %s\n", hash)
+
+	var circuit CubicCircuit
+	r1cs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &circuit)
+	if err != nil {
+		fmt.Printf("Compile failed : %v\n", err)
+		return
+	}
+
+	pk, vk, err := groth16.Setup(r1cs)
+	if err != nil {
+		fmt.Printf("Setup failed\n")
+		return
+	}
+
+	assignment := &CubicCircuit{PreImage: preImage, Hash: hash}
+	witness, _ := frontend.NewWitness(assignment, ecc.BN254)
+	proof, err := groth16.Prove(r1cs, pk, witness)
+	if err != nil {
+		fmt.Printf("Prove failed: %v\n", err)
+		return
+	}
+
+	publicWitness, _ := witness.Public()
+
+	f, _ := os.OpenFile("mimc_groth16.sol", os.O_CREATE|os.O_WRONLY, 0666)
+	defer f.Close()
+	vk.ExportSolidity(f)
+
+	err = groth16.Verify(proof, vk, publicWitness)
+	if err != nil {
+		fmt.Printf("verification failed: %v\n", err)
+		return
+	}
+	fmt.Printf("verification succeded\n")
+}
